7e0514062be97ef1a34feb1039cd747d2e16ba1b
Majid <mj_azani@hotmail.com>
2025-10-05 14:24:47 +1030

    ravale packages added

---
+++ laravel/laravel-packages.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get install sqlite3
+
+# packages to install on server
+# sudo apt install jpegoptim optipng pngquant gifsicle libavif-bin
+# sudo apt install nodejs npm -y
+# sudo npm install -g svgo
+# sudo npm install -g svgo
+# sudo apt install imagemagick libmagickwand-dev -y
+# sudo apt install php8.2-imagick -y
+# sudo apt install ghostscript -y # handling pdf files
